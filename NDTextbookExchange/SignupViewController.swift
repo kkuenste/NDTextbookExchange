5980a9e527b440477a107335927a1056fc782c4e
Katie Kuenster <katiekuenster@Katie-Kuensters-MacBook-Air.local>
2017-02-20 01:57:11 -0500

    first progress report milestone

--- NDTextbookExchange/SignupViewController.swift
+++ NDTextbookExchange/SignupViewController.swift
@@ -9,9 +9,50 @@
 import UIKit
 
 class SignupViewController: UIViewController {
-
-    @IBAction func doneButton(_ sender: Any) {
+    
+    @IBAction func cancelButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var venmoTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var verifyTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBAction func doneButton(_ sender: Any) {
+        if (emailTextField.text == "" || passwordTextField.text == "" || verifyTextField.text == "" || nameTextField.text == "" || phoneTextField.text == "" || venmoTextField.text == "") {
+            let alert = UIAlertController(title: "Empty Field", message: "You must fill in all text fields.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+        else if (emailTextField.text?.range(of: "@nd.edu") == nil) {
+            let alert = UIAlertController(title: "Notre Dame Email", message: "To create an account, you must use a Notre Dame email address.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+        else if (passwordTextField.text != verifyTextField.text) {
+            let alert = UIAlertController(title: "Check Password", message: "Your password must match in both text fields.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+        else if !(validate(value: phoneTextField.text!)) {
+            let alert = UIAlertController(title: "Invalid Phone Number", message: "Please enter a valid phone number.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+        else {
+            //let user = User(email: emailTextField.text!, password: passwordTextField.text!, name: nameTextField.text!, venmo: venmoTextField.text!)
+            
+            dismiss(animated: true, completion: nil)
+            
+        }
+        
     }
     
     override func viewDidLoad() {
@@ -35,5 +76,13 @@ class SignupViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // function to validate the user's phone number
+    func validate(value: String) -> Bool {
+        let PHONE_REGEX = "^\\d{3}-\\d{3}-\\d{4}$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
+        let result =  phoneTest.evaluate(with: value)
+        return result
+    }
 
 }
---
+++ NDTextbookExchange/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  NDTextbookExchange
+//
+//  Created by Katie Kuenster on 2/14/17.
+//  Copyright © 2017 CSE40814. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    var email: String
+    var password: String
+    var name: String
+    var venmo: String
+    
+    init( email: String, password: String, name: String, venmo: String ) {
+        self.email = email
+        self.password = password
+        self.name = name
+        self.venmo = venmo
+    }
+    
+}
+
