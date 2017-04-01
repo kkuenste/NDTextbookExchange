dd5d4f67479d5445f362f543d7b4d4dd7b88657c
Katie Kuenster <katiekuenster@Katie-Kuensters-MacBook-Air.local>
2017-04-01 15:09:46 -0400

    scans barcodes

---
+++ Pods/Target Support Files/RSBarcodes_Swift/RSBarcodes_Swift-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "ContextMaker.h"
+#import "RSBarcodes-Bridging-Header.h"
+
+FOUNDATION_EXPORT double RSBarcodes_SwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char RSBarcodes_SwiftVersionString[];
+
